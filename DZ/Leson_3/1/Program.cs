308361884727ea9a42cb8235ace7480dcaa3ce71
Dom <96082010@mail.ru>
2022-12-03 19:04:18 +0300

    011

---
+++ DZ/Leson_3/1/Program.cs
@@ -0,0 +1,14 @@
+﻿void Quarters(int x, int y)
+{
+    if (x == 0 || y == 0 )
+        Console.WriteLine("x = 0 or y = 0");
+    else if (x > 0 && y > 0 )
+        Console.WriteLine("1");
+    else if (x > 0 && y < 0 )
+        Console.WriteLine("4");    
+    else if (x < 0 && y > 0 )
+        Console.WriteLine("2");
+    else if (x < 0 && y < 0 )
+        Console.WriteLine("3");
+}
+Quarters(-1,9);
